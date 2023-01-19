ce39facfafe7ac13df4eef52129a318f1eaa9a66
DDidas <david.didas@aol.com>
2023-01-19 10:58:54 +0100

    Added files

--- fibonacci_sum.py
+++ fibonacci_sum.py
@@ -1,15 +1,30 @@
 def fibonacci(n):
+    a = 0
+
+    b = 1
+
     if n == 0:
-        return 0
+
+        return a
+
     elif n == 1:
-        return 1
+
+        return b
+
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
 
-def sum_fibonacci(n):
-    sum = 0
-    for i in range(n+1):
-        sum += fibonacci(i)
-    return sum
+        for i in range(2, n + 1):
+            c = a + b
 
-print(sum_fibonacci(n))
+            a = b
+
+            b = c
+
+        return b
+
+
+x = int(input("Enter a number: "))
+
+result = fibonacci(6)
+
+print(result)
